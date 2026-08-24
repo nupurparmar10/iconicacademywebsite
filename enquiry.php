52a0f48fb04a4b911ffbba8c46f16d8b5d2c3028
Nupur Parmar <63732953+nupurparmar10@users.noreply.github.com>
2026-08-24 12:20:39 +0530

    Remove SMTP password from enquiry.php
    Removed the SMTP password for security reasons.

--- enquiry.php
+++ enquiry.php
@@ -36,7 +36,7 @@
                 $mail->Host = 'smtp.gmail.com';
                 $mail->SMTPAuth = true;
                 $mail->Username = 'Iconickidssalon@gmail.com';
-                $mail->Password = 'nags bfcr obmm okbo';
+                $mail->Password = '';
                 $mail->SMTPSecure = 'tls';
                 $mail->Port = 587;
 
